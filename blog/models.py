26ff16fc944e5aa22877a87fce8ef85f2b0b5733
Yasin Javar <yasinjjjv@gmail.com>
2021-02-08 15:25:11 +0330

    first commit django app

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.user',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
