7eef1cac71494a239e3fd2f26b3785750b1438cf
Yasin Javar <yasinjjjv@gmail.com>
2021-02-17 09:32:05 +0330

    new test file

--- blog/tests.py
+++ blog/tests.py
@@ -18,8 +18,11 @@ class BlogTests(TestCase):
             author=self.user,
         )
 
+
 def test_get_absolute_url(self):
-    self.assertEqual(self.post.test_get_absolute_url(),'post/1')
+    self.assertEqual(self.post.test_get_absolute_url(), 'post/1')
+
+
 def test_string_representation(self):
 
     post = Post(title='A sample title')
@@ -47,3 +50,32 @@ def test_post_detail_view(self):
     self.assertEqual(no_response.status_code, 404)
     self.assertContains(response, 'A good title')
     self.assertTemplateUsed(response, 'post_detail.html')
+
+
+def test_post_create_view(self):  # new
+
+
+    response = self.client.post(reverse('post_new'), {
+        'title': 'New title',
+        'body': 'New text',
+        'author': self.user.id,
+    })
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, 'New title')
+    self.assertEqual(Post.objects.last().body, 'New text')
+
+
+def test_post_update_view(self):  # new
+
+
+    response = self.client.post(reverse('post_edit', args='1'), {
+        'title': 'Updated title',
+        'body': 'Updated text',
+    })
+    self.assertEqual(response.status_code, 302)
+    def test_post_delete_view(self):  # new
+
+
+    response = self.client.post(
+        reverse('post_delete', args='1'))
+    self.assertEqual(response.status_code, 302)
